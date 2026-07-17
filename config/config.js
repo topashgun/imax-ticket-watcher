ca4d03aa125e774b121760d051c4daae11cfc2c8
ashwinbala <ashwin.bala@influx.co.in>
2026-07-18 01:50:36 +0530

    updating config to check

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   movieId: "35098",
-  cinemaName: "PVR Palazzo-The Nexus Vijaya Mall",
-  experience: "IMAX",
+  cinemaName: "PVR Heritage RSL ECR Chennai",
+  experience: "",
   dates: ["2026-07-20", "2026-07-21", "2026-07-22"],
   pollIntervalMinutes: 1,
 };
